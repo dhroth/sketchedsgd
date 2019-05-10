2d114d94f69f90d2165be0c652d0477b50f675ff
Daniel R <drothchild@berkeley.edu>
2019-05-09 17:27:07 -0700

    Add fxn to show GPU memory use by tensors

--- sketchedsgd/sketched_optimizer.py
+++ sketchedsgd/sketched_optimizer.py
@@ -24,6 +24,29 @@ def topk(vec, k):
     ret[topkIndices] = vec[topkIndices]
     return ret
 
+def printMemoryUsage():
+    import gc
+    bigs = []
+    totalBytes = 0
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                print(type(obj), obj.size())
+                if isinstance(obj, torch.cuda.ByteTensor):
+                    dsize = 1
+                elif isinstance(obj, torch.cuda.FloatTensor) or isinstance(obj, torch.cuda.IntTensor):
+                    dsize = 4
+                elif isinstance(obj, torch.cuda.DoubleTensor) or isinstance(obj, torch.cuda.LongTensor):
+                    dsize = 8
+                totalBytes += np.product(obj.size()) * dsize
+                if obj.size()[0] > 90000000:
+                    bigs.append(obj)
+        except:
+            pass
+    for big in bigs:
+        print(big)
+    print("Total Size: {} MB".format(totalBytes / 1024 / 1024))
+
 
 class SketchedSGD(torch.optim.Optimizer):
     """SketchedSGD optimizer
